7cd8b53c1548159a23fda7331f7cc56f3c18f825
Andrew Yatsura <andrewyazura203@gmail.com>
2023-04-19 17:18:27 +0300

    add database connection

---
+++ user_service/database.py
@@ -0,0 +1,16 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from .config import settings
+
+client = AsyncIOMotorClient(
+    host=settings.mongo_host,
+    port=settings.mongo_port,
+    username=settings.mongo_user,
+    password=settings.mongo_pass,
+)
+
+database = client[settings.mongo_database_name]
+
+
+async def create_indexes() -> None:
+    await database["users"].create_index("telegram_id", unique=True)
